025b5d3647f0ca3a87de45ae2bcc8f3c2ff07fec
Vishakha Gautam <156678713+Vishakha2431@users.noreply.github.com>
2026-04-27 14:35:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", fetchValues);
+
+function fetchValues() {
+    let firstName = document.getElementById("first-name");
+    let lastName = document.getElementById("last-name");
+    let phoneNumber = document.getElementById("number");
+    let email = document.getElementById("email");
+
+    alert(`First Name: ${firstName.value} Last Name: ${lastName.value} Phone Number: ${phoneNumber.value} Email ID: ${email.value}`);
+
+    firstName.value = "";
+    lastName.value = "";
+    phoneNumber.value = "";
+    email.value = "";
+}
